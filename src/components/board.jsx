eb658bfa3b072a87d0989f5f1412e08f740ab74c
Jose Maria CL <chema9703@gmail.com>
2021-06-30 13:18:48 -0500

    Move the piece when clicking on a valid square position

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,9 +1,12 @@
 import { Square } from './square'
 import { Knight } from './knight'
+import { canMoveKnight, moveKnight } from '../observers/observe'
 
-function renderSquare(i, [knightX, knightY]) {
+function renderSquare(i, knightPosition) {
   const x = i % 8
   const y = Math.floor(i / 8)
+
+  const [knightX, knightY] = knightPosition
 
   const black = (x + y) % 2 === 1
   const isKnightHere = knightX === x && knightY === y
@@ -12,12 +15,20 @@ function renderSquare(i, [knightX, knightY]) {
   return (
     <div
       key={i}
+      onClick={() => handleSquareClick(x, y)}
       style={{ width: '50px', height: '50px' }}>
       <Square black={black}>
         {piece}
       </Square>
     </div>
   )
+}
+
+function handleSquareClick(toX, toY) {
+  console.log('trying to move the piece', { toX,  toY })
+  if (canMoveKnight(toX, toY)) {
+    moveKnight(toX, toY)
+  }
 }
 
 export function Board({ knightPosition = [0, 0] }) {
